f8a5e0c23f6f0ef64017a1d027cd8466f070eb9c
Choadis <dahlgrensm@gmail.com>
2018-02-09 13:10:47 -0500

    Took out sqlite and replaced with psql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180206094803) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "pokemons", force: :cascade do |t|
     t.string "Name"
     t.integer "Generation"
